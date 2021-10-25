1ba4546b3bf9a9b4c912f788b8fc59fd764f2a72
Anbiya Popal <anbiya2004@gmail.com>
2021-10-25 11:44:52 +0200

    add new charset and have code skip lines to compensate for the height of characters. also add a descriptive line written to file with basic data about the picture

--- ascii_convert.py
+++ ascii_convert.py
@@ -2,15 +2,15 @@ import os
 import argparse
 from PIL import Image, ImageEnhance
 
-# characters = ".,-~:;=*!#$@"
+# CHARSET = ".,-~:;=*!#$@"
+CHARSET = " .'`^,:;Il!i><~+_-?][}{1)(|\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 
 def value_to_ascii(value, characters):
-    # this takes a character's average value and returns an ASCII character. 
-
+    # This takes a value between 0 and 255 and returns a corresponding ASCII character
     for idx, char in enumerate(characters):
         if value < 255 / len(characters) * idx:
-            return f" {char}"
-    return "  "
+            return f"{char}"
+    return " "
 
 def ascii_convert(input_path, resize_x, resize_y, contrast_factor, flipped):
     img = Image.open(input_path)
@@ -23,14 +23,16 @@ def ascii_convert(input_path, resize_x, resize_y, contrast_factor, flipped):
 
     output = "\n"
     
-    for y in range(img.size[1]):
+    for idx, y in enumerate(range(img.size[1])):
+        if idx % 2 == 0:
+            continue
         for x in range(img.size[0]):
             pixel = img.getpixel((x, y)) # get pixel values in (r, g, b) shape
             average = (pixel[0] + pixel[1] + pixel[2]) / 3
             if flipped:
-                output += value_to_ascii(average, "@$#!*=;:~-,.")
+                output += value_to_ascii(average, CHARSET[::-1])
             else:
-                output += value_to_ascii(average, ".,-~:;=*!#$@")
+                output += value_to_ascii(average, CHARSET)
         output += '\n'
 
     output = output.split("\n")
@@ -49,7 +51,7 @@ def ascii_convert(input_path, resize_x, resize_y, contrast_factor, flipped):
 
     return string_output
 
-def write_to_file(text, input_path):
+def write_to_file(text, input_path, desc):
     # create output path if it doesn't exist yet
     if not os.path.exists('output/'):
         os.makedirs('output/')
@@ -60,7 +62,7 @@ def write_to_file(text, input_path):
     # create new file (or open it if it's already there), 
     # and write the output to it (it does not wipe previous output, you can compare them that way)
     output_file = open("output/{}.txt".format(filename), 'a')
-    # output_file.write(f"Size: {resize_x}, {resize_y} - Contrast: {contrast_factor}")
+    output_file.write(f"\nSize: {desc[0]}, {desc[1]} - Contrast factor: {desc[2]} - Flipped: {'Yes' if desc[3] else 'No'} - Character count: {len(text)}\n")
     output_file.write(text)
     output_file.close()
 
@@ -71,11 +73,11 @@ def main():
     parser.add_argument('-x', '--resize_x', type=int, required=True, help="The new size of the x-axis")
     parser.add_argument('-y', '--resize_y', type=int, required=True, help="The new size of the y-axis")
     parser.add_argument('-c', '--contrast', type=float, required=False, default=1, help="Factor of how much the contrast should change, >1 = increase, <1 = decrease")
-    parser.add_argument('-f', '--flipped', action="store_true", default=False, help="whether or not you want to flip/invert the color")
+    parser.add_argument('-f', '--flipped', action="store_true", required=False, default=False, help="Whether or not you want to flip/invert the color")
 
     args = parser.parse_args()
     text = ascii_convert(args.input, args.resize_x, args.resize_y, args.contrast, args.flipped)
-    write_to_file(text, args.input)
+    write_to_file(text, args.input, [args.resize_x, args.resize_y, args.contrast, args.flipped])
 
 if __name__ == "__main__":
     main()
